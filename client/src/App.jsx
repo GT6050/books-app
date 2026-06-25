8e7a63cb18a551301ecfe9cdf5273b1479b70744
Georgi Todorov <georgitodorov6050@gmail.com>
2026-06-26 01:50:24 +0300

    feat: setup react app with vite, tailwind, and routing

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import BookDetailPage from './pages/BookDetailPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import AddBookPage from './pages/AddBookPage';
+
+const App = () => {
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/books/:id' element={<BookDetailPage />} />
+					<Route path='/login' element={<LoginPage />} />
+					<Route path='/register' element={<RegisterPage />} />
+					<Route path='/add' element={<AddBookPage />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+};
+
+export default App;
