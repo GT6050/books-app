dfc116102c18746c49b8fdf8ac6d113de8dbe1f3
Georgi Todorov <georgitodorov6050@gmail.com>
2026-06-25 19:31:24 +0300

    connect PostgreSQL database with pg pool

--- server/index.js
+++ server/index.js
@@ -6,10 +6,9 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.get('/health', (req, res) => {
-	res.json({status: "ok"})
+	res.json({ status: 'ok' });
 });
 
 app.listen(PORT, () => {
 	console.log(`Listening on ${PORT}`);
 });
-
